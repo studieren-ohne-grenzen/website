15a59fdef4a3a1bf0fa7ada9212bc4eb02e15354
DimitriStibane <69897356+DimitriStibane@users.noreply.github.com>
2021-04-22 12:14:50 +0200

    Tiles

---
+++ components/landingTiles.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="grid gap-3 grid-cols-2">
+    <div v-for="(i, content) in contents" :key="i" class="border rounded-md">
+      {{ content }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingTiles',
+  data() {
+    return {
+      contents: [
+        'Test1',
+        'Test2',
+        'Test3',
+        'Test4',
+        'Test5',
+        'Test6',
+        'Test7',
+        'Test8',
+      ],
+    }
+  },
+}
+</script>
