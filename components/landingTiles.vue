5c2d84318a66bb27306f7052686df42bbd8de2be
DimitriStibane <69897356+DimitriStibane@users.noreply.github.com>
2021-05-07 18:24:11 +0200

    tiles_async fetch()

--- components/landingTiles.vue
+++ components/landingTiles.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="grid gap-3 grid-cols-2">
     <div
-      v-for="current_content in tiles"
-      :key="current_content.tiles"
+      v-for="tile in tiles"
+      :key="tile.name"
       class="border rounded-md border-sogblue-white hover:border-sogblue-lighter"
     >
       {{ current_content.id }}
       <nuxt-picture
-        :src="tiles.image"
-        :alt="tiles.name"
+        :src="tile.image"
+        :alt="tile.name"
         quality="80"
         format="webp"
         loading="lazy"
@@ -21,27 +21,32 @@
 
 <script>
 export default {
-  name: 'Tiles',
+  name: 'LandingTiles',
   props: {
-    tiles: {
-      type: Object,
-      default() {
-        return {
-          tiles: [
-            { id: 1, name: 'test1' },
-            { id: 2, name: 'test2' },
-            { id: 3, name: 'test3' },
-            { id: 4, name: 'test4' },
-            { id: 5, name: 'test5' },
-            { id: 6, name: 'test6' },
-            { id: 7, name: 'test7' },
-          ],
-        }
-      },
+    tilesConfig: {
+      type: String,
+      default: 'tiles',
     },
+  },
+  async fetch() {
+    const tiles = await this.$content(
+      `${this.$i18n.locale}`,
+      this.tilesConfig
+    ).fetch().imgs
+    console.log(tiles)
+    return { tiles }
   },
 }
 /**
+ *  props: {
+    tilesConfig: {
+      type: String,
+      default: '',
+    },
+  },
+ * 
+ console.log(tiles)
+ * 
         { id: 1, name: 'test1' },
         { id: 2, name: 'test2' },
         { id: 3, name: 'test3' },
