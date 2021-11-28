02005148dd358f4e0d8472dafd82847902d17c13
Alex Francklin Wobiwo Fowe <francklinfowe@yahoo.com>
2021-11-28 20:11:05 +0100

    :sparkles: Iframe component created

---
+++ components/externalContent.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="w-auto h-full">
+    <!-- Protection -->
+    <div
+      v-if="confirm"
+      class="w-1/2 p-4 my-auto mx-auto border border-gray-400"
+    >
+      <p class="pb-4">
+        {{ message }}
+      </p>
+      <input
+        type="button"
+        class="button"
+        value="Zustimmen"
+        @click="confirm = !confirm"
+      />
+    </div>
+
+    <!-- IFrame -->
+    <iframe
+      v-if="!confirm"
+      ref="myIframe"
+      :src="`${url}`"
+      :width="`${width}`"
+      :height="`${height}`"
+      title="Iframe Example"
+      class="my-auto mx-auto border border-gray-400"
+    ></iframe>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ExternalContent',
+  props: {
+    url: {
+      type: String,
+      default: '',
+    },
+    width: {
+      type: String,
+      default: '100%',
+    },
+    height: {
+      type: String,
+      default: '1024px',
+    },
+    message: {
+      type: String,
+      default: '',
+    },
+  },
+  data: () => ({
+    confirm: false,
+  }),
+  beforeMount() {
+    this.confirm = true
+  },
+}
+</script>
+<style lang="postcss">
+.nuxt-content .button {
+  @apply bg-sogblue border-0 text-white px-4 py-2 text-center cursor-pointer inline-block;
+}
+</style>
--- content/de/shop/index.md
+++ content/de/shop/index.md
@@ -1,3 +1,5 @@
 ---
 title: Shop
 ---
+
+<external-content width="100%" height="4000px" url="https://www.studieren-ohne-grenzen.org/unterstuetzen/merch/#!/" message="Officia est reprehenderit enim elit cupidatat fugiat ad magna ullamco nostrud aliquip aliquip eiusmod. Aliqua labore commodo et laboris velit enim aute magna deserunt sint culpa. Consectetur aliqua irure quis non exercitation officia ad ipsum velit elit ea commodo nostrud."></external-content>
