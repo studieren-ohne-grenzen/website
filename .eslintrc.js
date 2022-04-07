e4cbe32a811907f343c1c4c0c96987c0539252b2
wanLo <mail@geahnert.de>
2022-04-07 14:27:02 +0200

    Re-enable linting with eslint & prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,10 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
   plugins: ['prettier'],
   // add your custom rules here
   rules: {},
